7a2cdf27d1b3cadb716910400e1aa8a406d90896
Costa1515 <116725667+Costa1515@users.noreply.github.com>
2022-11-16 11:36:21 +0000

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        int[] array1 = {2, 9, 6, 4};
+        int[] array2 = {0, 3, 1, 8};
+        int[] array3 = {7, 5, 3, 5};
 
+
+        int[][]arrays = {array1, array2, array3};
+        for(int[] array: arrays) {
+            for(int i: array) {
+                System.out.print(i +" ");
+            }
+            System.out.print(" ");
+        }
 
     }
 }
